09a5ccfa4b2a5a45d951fca70ef548c467f83e84
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-09-17 15:13:32 +0200

    Added tailwiind

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Switch } from 'react-router-dom';
-import "./App.css";
-import PublicRoute from './routes/publicRoutes';
-
+import { BrowserRouter, Switch } from "react-router-dom";
+import "./index.css";
+import PublicRoute from "./routes/publicRoutes";
+import PrivateRoute from "./routes/privateRoutes";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <PublicRoute restricted={false} component={Home} path="/" exact />
+        <PublicRoute restricted={false} component={Home} path="/home" exact />
         <PrivateRoute component={Dashboard} path="/dashboard" exact />
       </Switch>
     </BrowserRouter>
@@ -15,11 +15,21 @@ export default function App() {
 }
 
 const Home = () => (
+  <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+    <div className="flex-shrink-0">
+      <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+    </div>
+    <div>
+      <div className="text-xl font-medium text-black">ChitChat</div>
+      <p className="text-gray-500">You have a new message!</p>
+    </div>
+  </div>
+);
+
+const Dashboard = () => (
   <div className="App">
     <header className="App-header">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
+      <p>Hello World from Dashboard</p>
       <a
         className="App-link"
         href="https://reactjs.org"
