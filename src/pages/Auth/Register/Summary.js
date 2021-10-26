4ead8e5be2062d9e148d5b5de657bd0ce2ea7c48
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-10-26 18:04:31 +0200

    Finetuning summary

--- src/pages/Auth/Register/Summary.js
+++ src/pages/Auth/Register/Summary.js
@@ -4,7 +4,7 @@ import { setStep } from "actions/register.action";
 
 const Summary = ({ profile, recap, setStep }) => {
   const handleSubmit = () => {
-    setStep("email")
+    setStep("email");
   };
   return (
     <div>
@@ -27,48 +27,50 @@ const Summary = ({ profile, recap, setStep }) => {
           <p className="w-2/3 text-md mb-8 font-light text-dark-500 font-gibson tracking-wide">
             Voici à quoi ressemblera ton profil
           </p>
-          <div className="bg-white shadow-md h-96 mx-3 rounded-3xl flex flex-col justify-around items-center overflow-hidden sm:flex-row sm:h-52 sm:w-3/5">
-            <img
-              className="h-1/2 w-full sm:h-full sm:w-1/2 object-cover"
-              src={recap.photo[0].data_url}
-              alt="profile_picture"
-            />
-            <div className="flex-1 w-full flex flex-col items-baseline justify-around h-1/2 pl-6 sm:h-full sm:items-baseline sm:w-1/2">
-              <div className="flex flex-col justify-start items-baseline">
-                <h1 className="text-lg font-normal mb-0 text-gray-600 font-sans">
-                  {recap.firstName} - {recap.lastName}
-                </h1>
-                <span className="text-md font-gibson font-semibold text-primary-300 mt-0">
-                  {recap.bio}
-                </span>
-              </div>
-              <div className="flex mt-10 w-full">
-                {recap.location?.presentiel && (
-                  <div className="py-1 px-4 mr-3 bg-primary-300 rounded-full">
-                    <p className="text-white font-semibold font-gibson">
-                      Paris
-                    </p>
-                  </div>
-                )}
-                {recap.location?.presentiel && (
-                  <div className="py-1 mr-5 px-4 bg-secondary-400 rounded-full">
-                    <p className="text-white font-semibold font-gibson">
-                      Visio
-                    </p>
-                  </div>
-                )}
+          <div className="flex">
+            <div className="bg-white shadow-md h-96 mx-3 rounded-3xl flex flex-col justify-around items-center overflow-hidden sm:flex-row sm:h-52 sm:w-3/5">
+              <img
+                className="h-1/2 w-full sm:h-full sm:w-1/2 object-cover"
+                src={recap.photo[0].data_url}
+                alt="profile_picture"
+              />
+              <div className="flex-1 w-full flex flex-col items-baseline justify-around h-1/2 pl-6 sm:h-full sm:items-baseline sm:w-1/2">
+                <div className="flex flex-col justify-start items-baseline">
+                  <h1 className="text-lg font-normal mb-0 text-gray-600 font-sans">
+                    {recap.firstName} - {recap.lastName}
+                  </h1>
+                  <span className="text-md font-gibson font-semibold text-primary-300 mt-0">
+                    {recap.bio}
+                  </span>
+                </div>
+                <div className="flex mt-10 w-full">
+                  {recap.location?.presentiel && (
+                    <div className="py-1 px-4 mr-3 bg-primary-300 rounded-full">
+                      <p className="text-white font-semibold font-gibson">
+                        Paris
+                      </p>
+                    </div>
+                  )}
+                  {recap.location?.presentiel && (
+                    <div className="py-1 mr-5 px-4 bg-secondary-400 rounded-full">
+                      <p className="text-white font-semibold font-gibson">
+                        Visio
+                      </p>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex mt-10">
-            <div className="bg-white shadow-md px-10 py-5 rounded-3xl">
-              <ul className="list-disc">
-                {recap.details.map((element) => (
-                  <li className="font-gibson text-xl text-dark-500">
-                    {element}
-                  </li>
-                ))}
-              </ul>
+            <div className="flex justify-between">
+              <div className="bg-white shadow-md px-10 py-5 rounded-3xl">
+                <ul className="list-disc">
+                  {recap.details.map((element) => (
+                    <li className="font-gibson text-xl text-dark-500">
+                      {element}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
 
