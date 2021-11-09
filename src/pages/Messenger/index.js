83e0416d92f285859751df7a70f919b296dd1c8a
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-11-09 20:00:07 +0100

    SA-58 created different pages

---
+++ src/pages/Messenger/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Messenger = () => {
+  return (
+    <div>
+      <p>Hello World from messenger</p>
+    </div>
+  )
+}
+export default Messenger
---
+++ src/pages/Settings/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Settings = () => {
+  return (
+    <div>
+      <p>Hello World from Settings</p>
+    </div>
+  )
+}
+
+export default Settings
