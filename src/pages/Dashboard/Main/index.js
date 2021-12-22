475a9a92977cf62fbb30abf807326513f212ac48
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-12-22 23:07:08 +0100

    updated dashboard

--- src/pages/Dashboard/Main/index.js
+++ src/pages/Dashboard/Main/index.js
@@ -3,34 +3,36 @@ import React, { Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import Loader from "components/ui/loader";
+import ErrorPage from "pages/404";
 
-const Main = ({ routes, isLoading }) => {
+const Main = ({ routes, isLoading, auth }) => {
   return (
     <main className="h-full bg-purple-50 overflow-y-auto">
       <div className="relative grid">
         <Suspense fallback={<ThemedSuspense />} />
-        <Loader isLoading={isLoading}/>
-          <Switch>
-            {routes.map((route, i) => {
-              return route.component ? (
-                <Route
-                  key={i}
-                  exact={true}
-                  path={`/app${route.path}`}
-                  render={(props) => <route.component {...props} />}
-                />
-              ) : null;
-            })}
-            {/* <Redirect exact from="/app" to="/" /> */}
-            {/* <Route component={Page404} /> */}
-          </Switch>
+        <Loader isLoading={isLoading} />
+        <Switch>
+          {routes.map((route, i) => {
+            return route.component ? (
+              <Route
+                key={i}
+                exact={true}
+                path={`/app${route.path}`}
+                render={(props) => <route.component {...props} />}
+              />
+            ) : null;
+          })}
+          {/* <Redirect exact from="/app" to="/" /> */}
+          {/* <Route from="*" to="/404" component={ErrorPage} /> */}
+        </Switch>
       </div>
     </main>
   );
 };
 
 const mapStateToProps = (state) => ({
-  isLoading: state.UI.isLoading
+  isLoading: state.UI.isLoading,
+  auth: state.Auth
 });
 
 export default connect(mapStateToProps)(Main);
