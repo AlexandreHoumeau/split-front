37e040635cfb823e86f13e2fd9c88b420d371165
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-01-08 22:43:20 +0100

    Updated Input component

--- src/components/settings/input.js
+++ src/components/settings/input.js
@@ -1,29 +1,38 @@
+import classNames from "classnames";
 import React from "react";
 
 const Input = ({
   handleChange,
+  disable,
   id,
   placeholder,
   handleBlur,
   value,
+  defaultValue,
   label,
   error,
-  type
+  type,
 }) => {
   return (
     <>
       <div
-        className={`text-sm font-gibson mt-10 font-semibold ${
-          error ? "text-red-500" : "text-dark-500"
-        } tracking-wide`}
+        className={classNames(
+          "tracking-wide text-sm font-gibson mt-10 font-semibold",
+          error ? "text-red-500" : "text-dark-500",
+          disable ? "text-gray-300" : ""
+        )}
       >
         {label}
       </div>
       <input
-        className={`w-full font-gibson text-lg rounded p-4 border ${
-          error ? "border-red-300" : "border-primary-500"
-        } focus:outline-none focus:border-primary-500`}
-        type={type ? type : 'text'}
+        disabled={disable}
+        className={classNames(
+          "w-full font-gibson text-lg rounded p-4 border focus:outline-none focus:border-primary-500",
+          error ? "border-red-300" : "border-primary-500",
+          disable ? "bg-gray-100" : ""
+        )}
+        type={type ? type : "text"}
+        defaultValue={defaultValue}
         id={id}
         name={id}
         placeholder={placeholder}
