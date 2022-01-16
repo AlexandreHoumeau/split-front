392786358f492c1e27ae385f6cef67a79194e19f
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-01-16 17:51:12 +0100

    updated contacts

--- src/pages/Messenger/Contacts/index.js
+++ src/pages/Messenger/Contacts/index.js
@@ -1,31 +1,40 @@
+import classNames from "classnames";
 import Avatar from "components/ui/Avatar";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { connect } from "react-redux";
+import { useHistory } from "react-router";
 import api from "services/api";
 import UserCard from "./list";
 
 const Contacts = ({ user }) => {
-  const [contacts, setContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
+  const [selectedConversation, setSelectedConversation] = useState();
+  const history = useHistory();
 
+  const selectConversation = (conversationId) => {
+    setSelectedConversation(conversationId);
+    history.push(`/app/messenger/${conversationId}`);
+  };
   const fetchContacts = async () => {
-    const data = await api.axios.get('/v1/conversation/contact')
-    console.log(data)
+    const data = await api.axios.get("/v1/conversation/contact");
 
     if (data.contacts) {
-      setContacts(data.contacts)
+      setContacts(data.contacts);
     }
-  }
+  };
   useEffect(() => {
-    fetchContacts()
-  }, [])
+    fetchContacts();
+  }, []);
 
   return (
-    <div className="bg-white shadow-lg rounded-xl px-10 py-9 font-gibson">
+    <div className="bg-white flex-1 shadow-lg rounded-xl px-10 py-9 font-gibson">
       <div className="flex">
-        <Avatar picture={user.picture} />
+        <Avatar displayTag={true} picture={user.picture} />
         <div className="ml-3">
-          <div className="text-lg text-dark-500 font-semibold">{user.firstName} {user.lastName}</div>
+          <div className="text-lg text-dark-500 font-semibold">
+            {user.firstName} {user.lastName}
+          </div>
           <div className="text-primary-500">Connecté(e)</div>
         </div>
       </div>
@@ -35,12 +44,28 @@ const Contacts = ({ user }) => {
       </div>
       <div className="bg-gray-300 w-full h-px" />
 
-      <div className="mt-5">
+      <div className="mt-5 overflow-y-scroll h-full">
         {!contacts?.length ? (
-          <div className="italic text-gray-400 text-center">Vous n'avez pas de conversation</div>
-        ): (
-          contacts.map((contact) => (
-            <UserCard contact={contact} />
+          <div className="italic text-gray-400 text-center">
+            Vous n'avez pas de conversation
+          </div>
+        ) : (
+          contacts.map((contact, index) => (
+            <div onClick={() => selectConversation(contact._id)} key={index}>
+              <div
+                className={classNames(
+                  "cursor-pointer p-2 rounded-xl transform duration-300",
+                  selectedConversation === contact._id
+                    ? "bg-gray-100 border border-gray-300"
+                    : ""
+                )}
+              >
+                <UserCard contact={contact} />
+              </div>
+              {index + 1 < contacts.length && (
+                <div className="bg-gray-300 my-3 w-full h-px" />
+              )}
+            </div>
           ))
         )}
       </div>
