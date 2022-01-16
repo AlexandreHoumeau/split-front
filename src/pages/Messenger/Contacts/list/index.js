e2432a777deded06b47edab35f1eef65eaa7f4af
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-01-16 17:51:23 +0100

    contacts list updated

--- src/pages/Messenger/Contacts/list/index.js
+++ src/pages/Messenger/Contacts/list/index.js
@@ -1,15 +1,25 @@
-import Avatar from 'components/ui/Avatar'
-import React from 'react'
+import Avatar from "components/ui/Avatar";
+import React from "react";
 
 const UserCard = ({ contact }) => {
-  console.log(contact)
   return (
     <div>
       <div className="flex">
-        <Avatar picture={contact.picture} />
+        <Avatar displayTag={false} size="14" picture={contact.picture} />
+        <div className="ml-2">
+          <div className="font-semibold text-lg text-dark-500">{contact.firstName}</div>
+          {contact.lastMessage ? (
+            <div>{contact.lastMessage.content}</div>
+          ) : (
+            <div className="text-gray-400 italic">Aucun message envoyé</div>
+          )}
+        </div>
+        <div>
+          {contact.lastMessage?._sentAt}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
