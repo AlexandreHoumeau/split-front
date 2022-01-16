03138c8643da845856aed75ee7ddd1d39450bf6c
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-01-16 17:50:53 +0100

    updated Messenger

--- src/pages/Messenger/index.js
+++ src/pages/Messenger/index.js
@@ -1,20 +1,32 @@
-import React from 'react'
-import Contacts from './Contacts'
+import React, { useEffect } from "react";
+import Contacts from "./Contacts";
+import MessagesView from "./MessagesView";
+import { useParams } from "react-router-dom";
 
-const Messenger = () => {
+const Messenger = (props) => {
+  let { conversationId } = useParams();
+
+  useEffect(() => {}, []);
   return (
     <div className="px-14 py-4">
+      <div className="grid grid-cols-8 space-x-8">
+        <div className="col-span-2">
+          <Contacts />
+        </div>
 
-      <div className="grid grid-cols-3 space-x-8">
-      <div className="col-span-1">
-        <Contacts />
-      </div>
-
-      <div className="col-span-2">
-
-      </div>
+        <div className="col-span-6 h-full">
+          {conversationId ? (
+            <MessagesView />
+          ) : (
+            <div className="flex justify-center">
+              <div className="text-gray-400 italic">
+                Selectionne une conversation
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
-  )
-}
-export default Messenger
+  );
+};
+export default Messenger;
