cf67c88e8b96c01a1c8268e3e5df6e2a377edba7
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-01-21 17:16:18 +0100

    updated socket service

--- src/services/socket/index.js
+++ src/services/socket/index.js
@@ -1,19 +1,40 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { io } from "socket.io-client";
 const { API_DOMAIN } = require("config");
-const { Manager } = require("socket.io-client");
 
-const manager = new Manager(API_DOMAIN, {
-  reconnectionDelayMax: 10000,
-  extraHeaders: {
-    Authorization: localStorage.getItem("authToken"),
-  },
-})
+const socket = io(API_DOMAIN, { autoConnect: false, reconnectionDelayMax: 10000 });
 
-const socket = manager.socket("/chat", {
-  auth: {
-    token: "123"
+socket.onAny((event, ...args) => {
+  console.log(event, args);
+});
+
+socket.on("connect_error", (err) => {
+  if (err.message === "invalid username") {
+    console.log('Error from socket')
   }
 });
 
 
-export default socket
+export default socket;
 
+// const manager = new Manager(API_DOMAIN, {
+//   reconnectionDelayMax: 10000,
+//   // autoConnect: false,
+//   extraHeaders: {
+//     Authorization: localStorage.getItem("authToken"),
+//   },
+// })
+
+
+// manager.open((err) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log('Successfull connection')
+//   }
+// });
+
+// const socket = manager.socket('/')
+
+
+// export default manager
