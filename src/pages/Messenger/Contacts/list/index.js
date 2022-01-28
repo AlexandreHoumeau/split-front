2e52de68d06693bae1ce06fec69330ba76e2cbd3
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-01-28 19:37:13 +0100

    updated list contact

--- src/pages/Messenger/Contacts/list/index.js
+++ src/pages/Messenger/Contacts/list/index.js
@@ -2,23 +2,31 @@ import classNames from "classnames";
 import Avatar from "components/ui/Avatar";
 import { connect } from "react-redux";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const UserCard = ({ contact, user }) => {
-  console.log(user)
+  const [card, setCard] = useState()
+
+  useEffect(() => {
+    setCard(contact)
+    console.log('Update card')
+  }, [contact, contact?.lastMessage])
+
   return (
     <div>
       <div className="flex">
-        <Avatar displayTag={false} size="14" picture={contact.picture} />
+        <Avatar displayTag={false} size="14" picture={card?.picture} />
         <div className="ml-2 font-gibson">
-          <div className="font-semibold text-lg text-dark-500">{contact.firstName}</div>
-          {contact.lastMessage ? (
-            <div className={classNames(contact.lastMessage.seenBy.includes(user._id) ? 'text-gray-400' : 'font-semibold', 'text-sm')}>{contact.lastMessage.content.substring(0, 18)}...</div>
+          <div className="font-semibold text-lg text-dark-500">{card?.firstName}</div>
+          {card?.lastMessage ? (
+            <div className={classNames(card?.lastMessage.seenBy.includes(user._id) ? 'text-gray-400' : 'font-semibold', 'text-sm')}>{card?.lastMessage.content.substring(0, 18)}...</div>
           ) : (
             <div className="text-gray-400 italic">Aucun message envoyé</div>
           )}
         </div>
         <div>
-          {contact.lastMessage?._sentAt}
+          {card?.lastMessage?._sentAt}
         </div>
       </div>
     </div>
