7197b5b89a45ff4f8efd99eed099962138a4a5e6
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-01-31 22:34:22 +0100

    updated mySpace

--- src/pages/Myspace/index.js
+++ src/pages/Myspace/index.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import api from "services/api";
+import Counter from "./teacher/counter";
 
-const MySpace = () => {
+const MySpace = ({ user }) => {
+
+  const getTeacherInformations = async () => {
+    const { teacher } = await api.axios.get('/v1/me/informations')
+    console.log(teacher)
+  }
+  useEffect(() => {
+    getTeacherInformations()
+  }, [])
   return (
     <div>
-      <p>Hello World from MySpace</p>
+      {user.profile === 'teacher' && <div>{<Counter />}</div>}
+      <div>This is the part of the student</div>
     </div>
   )
-}
+};
 
-export default MySpace
+const mapStateToProps = (state) => ({
+  user: state.Auth.user,
+});
+
+export default connect(mapStateToProps)(MySpace);
