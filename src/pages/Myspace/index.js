1a25b10e58bd3cb43409c0a32d5e9b1d56912268
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-02-07 22:21:25 +0100

    Updated myspace page and controller

--- src/pages/Myspace/index.js
+++ src/pages/Myspace/index.js
@@ -1,24 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { connect } from "react-redux";
 import api from "services/api";
 import Counter from "./teacher/counter";
 
 const MySpace = ({ user }) => {
+  const [teacherInformations, setTeacherInformations] = useState();
 
   const getTeacherInformations = async () => {
-    const { teacher } = await api.axios.get('/v1/me/informations')
-    console.log(teacher)
-  }
+    const data = await api.axios.get("/v1/me/informations");
+    setTeacherInformations(data);
+  };
   useEffect(() => {
-    getTeacherInformations()
-  }, [])
+    getTeacherInformations();
+  }, []);
   return (
     <div>
-      {user.profile === 'teacher' && <div>{<Counter />}</div>}
+      {user.profile === "teacher" && (
+        <div className="flex ml-10 mt-10">
+          {<Counter information={teacherInformations} />}
+        </div>
+      )}
       <div>This is the part of the student</div>
     </div>
-  )
+  );
 };
 
 const mapStateToProps = (state) => ({
--- src/pages/Myspace/teacher/counter.js
+++ src/pages/Myspace/teacher/counter.js
@@ -1,7 +1,38 @@
+import { CheckMarkIcon } from "assets/icons";
 import React from "react";
 
-const Counter = () => {
-  return <div>This is cournte r componetn</div>;
+const Counter = ({ information }) => {
+  console.log(information)
+  return (
+    <div className="font-gibson mb-3">
+      <div className="text-dark-500 font-semibold text-2xl">
+        Tes compteurs à date
+      </div>
+      <div className="bg-white p-5 mt-5 rounded-lg">
+        <div className="flex space-x-8 text-xl text-dark-500">
+          <div className="text-center">
+            <div>Formations données</div>
+            <div className="text-primary font-semibold text-6xl my-5">{information?.givenCourses}</div>
+          </div>
+
+          <div className="text-center">
+            <div>Formations à venir</div>
+            <div className="text-secondary font-semibold text-6xl my-5">{information?.furtherCourses}</div>
+          </div>
+
+          <div>
+            <div className="text-center">Tes formations avec le plus de succès</div>
+            {information?.topCourses?.map((course) => (
+            <div key={course.name} className="flex items-center">
+              <CheckMarkIcon className="ml-2 mb-2" />
+              <div>{course}</div>
+            </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
