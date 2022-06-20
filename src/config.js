a49f23111d89c8089866f5dfb468dec1c606fe0b
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-06-20 21:39:37 +0200

    test

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_DOMAIN = process.env.DOMAIN || 'http://localhost:3002'
+const API_DOMAIN = 'http://13.38.87.245:3002/'
 const JWT_TOKEN = 'authToken'
 const I18N_LANGUAGE = 'i18nextLng'
 const STRIPE_PUBLIC = process.env.REACT_APP_STRIPE_PUBLIC
